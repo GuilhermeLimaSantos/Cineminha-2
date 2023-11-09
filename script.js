a7f1ce68fc5d1d443502ad73f86238c55d46aebb
Guilherme H. l. dos Santos <125158701+GuilhermeLimaSantos@users.noreply.github.com>
2023-11-09 18:22:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,172 +1,169 @@
 const movies = [
-   {
-    title: "Encanto",
-    category: "Comédia",
-    imageSrc: "https://play-lh.googleusercontent.com/E05H_CCcoTRnq69ZoCXph2U8ZN6TJ8Us4SBpyFY9T11XD5H3Q4dd2rr3JZyl0LdcKHIXJvog89YhR55dw2IA"
-    },
 
-    {
-      title: "Moana",
-      category:"Fantasia",
-      imageSrc: "https://upload.wikimedia.org/wikipedia/pt/4/46/Moana_movie_poster_p_2016.jpg"
-    },
+  { name: "Encanto", 
+  image: "https://play-lh.googleusercontent.com/E05H_CCcoTRnq69ZoCXph2U8ZN6TJ8Us4SBpyFY9T11XD5H3Q4dd2rr3JZyl0LdcKHIXJvog89YhR55dw2IA", 
+  tickets: 100, 
+  category: "Comédia" },
 
-    {
-      title: "Red: Crescer é uma Fera",
-      category:"Amadurecimento",
-      imageSrc: "https://br.web.img3.acsta.net/pictures/21/11/18/16/57/5746772.jpg"
-    },
+  { name: "Moana", 
+  image: "https://upload.wikimedia.org/wikipedia/pt/4/46/Moana_movie_poster_p_2016.jpg", 
+  tickets: 150, 
+  category: "Comédia" },
 
-    {
-      title: "Elementos",
-      category:"Ficção",
-      imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDdab6m1R38wYdsAjwcuGacJ80G27bD5oV0HlVR6aJoTS_3S3v"
-    },
+  { name: "Red: Crescer é uma Fera", 
+  image: "https://br.web.img3.acsta.net/pictures/21/11/18/16/57/5746772.jpg", 
+  tickets: 80, 
+  category: "Comédia" },
 
-    {
-      title: "Barbie",
-      category:"Romance",
-      imageSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQfrQw_ilMVXBYmS4Cnen202Sqmuy7l1o9eMy04Ba1DCYvF367-"
-    },
+  { name: "Elementos", 
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDdab6m1R38wYdsAjwcuGacJ80G27bD5oV0HlVR6aJoTS_3S3v", 
+  tickets: 120, 
+  category: "Terror" },
 
-    {
-      title: "Super Mario Bros",
-      category:"Comédia",
-      imageSrc: "https://dx35vtwkllhj9.cloudfront.net/universalstudios/super-mario-bros/images/regions/us/onesheet.jpg"
-    },
+  { name: "Barbie", 
+  image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQfrQw_ilMVXBYmS4Cnen202Sqmuy7l1o9eMy04Ba1DCYvF367-", 
+  tickets: 90, 
+  category: "Comédia" },
 
-    {
-      title: "Nimona",
-      category:"Fantasia",
-      imageSrc: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQg_qPNpCFkfh8m00LkjVgk3j7dEjpuowyGHfBQC8XL0l8V9zLZ"
-    },
+  { name: "Super Mario Bros", 
+  image: "https://dx35vtwkllhj9.cloudfront.net/universalstudios/super-mario-bros/images/regions/us/onesheet.jpg", 
+  tickets: 110, 
+  category: "Comédia" },
 
-    {
-      title: "Miraculous",
-      category:"Aventura",
-      imageSrc: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRyGjVKaP42JztCmLEYI39MKnQ193jdZfV-bqob17_D5xrrAVPz"
-    },
+  { name: "Nimona", 
+  image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQg_qPNpCFkfh8m00LkjVgk3j7dEjpuowyGHfBQC8XL0l8V9zLZ", 
+  tickets: 70, 
+  category: "Comédia" },
 
-    {
-      title: "A Chamada",
-      category:"Ação",
-      imageSrc: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSjDWJcz1ONJrRXRuEcn8rkZklqAKFbtGVVrG37nZg6H60I-ZrZ"
-    },
+  { name: "Miraculous", 
+  image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRyGjVKaP42JztCmLEYI39MKnQ193jdZfV-bqob17_D5xrrAVPz", 
+  tickets: 140, 
+  category: "Romance" },
 
-    {
-      title: "Guardiões da Galáxia",
-      category:"Animação",
-      imageSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT1P_hV6E3_OlEq4MdFMQje5SyBUkAH0JKasP_iVJG4HMT69xn2"
-    },
+  { name: "A Chamada", 
+  image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSjDWJcz1ONJrRXRuEcn8rkZklqAKFbtGVVrG37nZg6H60I-ZrZ", 
+  tickets: 60, 
+  category: "Terror" },
 
-    {
-      title: "A Noite das Bruxas",
-      category:"Terror",
-      imageSrc: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-NvZ4l6sGpsZgfrC1sgIuXR3nN9sknbiUvZSG8IUkoxTjaeZo"
-    },
-    
-    {
-      title: "Dino King - Viagem à Montanha do Fogo ",
-      category:"Animação",
-      imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv0ijLRGlMa3Us9ThvKzDnjvK1YFQE-h1N58NF6-ODFkxJ544w"
-    },
-    
-    {
-      title: "Os Guardiões",
-      category:"Drama",
-      imageSrc: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZyDeXBdAZu-5gl_3YcUUL2HL3-yDLMHZzojkdK8I3rtd5BRI9"
-    },
-    
-    {
-      title: "Besouro Azul",
-      category:"Ação",
-      imageSrc: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTLXZb6z_VGWUc5O-s7iMJ8QVWQNa3b6hp6HawTsbW8dOJ1RQxh"
-    },
-    
-    {
-      title: "Adão Negro",
-      category:"Ação",
-      imageSrc: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSrULQHUpm3Zr60AXFAltaQkpo6DAe8FZmhSrYsa7t5DCUeaeF9"
-    },
-    
-    {
-      title: "Terapia do Medo",
-      category:"Terror",
-      imageSrc: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQrFxpYCPPWiiOGG5pZttHFKywlfTzHHXPYKojERotZLpsiMO42"
-    },
-    
-    {
-      title: "O Encontro de Carl",
-      category:"Aventura",
-      imageSrc: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQWn1yIZNI-6zpCqPFLd4mn0suKix0BUqxSqlb8yM2oleD8td9w"
-    },
-    
-    {
-      title: "M3GAN",
-      category:"Terror",
-      imageSrc: "https://dx35vtwkllhj9.cloudfront.net/universalstudios/megan/images/regions/us/onesheet.jpg"
-    },
-    
-    {
-      title: "2067",
-      category:"Cyberpunk",
-      imageSrc: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQUy7Dq51oPVZwnVzeDZUGDJWPRsYaSB2sN5F7Q0T2loxbJ0E0e"
-    },
+  { name: "Guardiões da Galáxia", 
+  image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT1P_hV6E3_OlEq4MdFMQje5SyBUkAH0JKasP_iVJG4HMT69xn2", 
+  tickets: 130, 
+  category: "Terror" },
 
-    {
-      title: "Paralisia",
-      category:"Ficção Científica",
-      imageSrc: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQDv-CKwtsWxfwG1I7CBPgVz8J8leuP059DWIDSPSzO8TtLOHfW"
-    }
+  { name: "Dino King - Viagem à Montanha do Fogo ", 
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv0ijLRGlMa3Us9ThvKzDnjvK1YFQE-h1N58NF6-ODFkxJ544w", 
+  tickets: 95, 
+  category: "Comédia" },
+
+  { name: "A Noite das Bruxas", 
+  image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT-NvZ4l6sGpsZgfrC1sgIuXR3nN9sknbiUvZSG8IUkoxTjaeZo", 
+  tickets: 105, 
+  category: "Terror" },
+
+  { name: "Os Guardiões", 
+  image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZyDeXBdAZu-5gl_3YcUUL2HL3-yDLMHZzojkdK8I3rtd5BRI9", 
+  tickets: 75, 
+  category: "Comédia" },
+
+  { name: "Besouro Azul", 
+  image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTLXZb6z_VGWUc5O-s7iMJ8QVWQNa3b6hp6HawTsbW8dOJ1RQxh", 
+  tickets: 125, 
+  category: "Comédia" },
+
+  { name: "Adão Negro", 
+  image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSrULQHUpm3Zr60AXFAltaQkpo6DAe8FZmhSrYsa7t5DCUeaeF9", 
+  tickets: 85, 
+  category: "Comédia" },
+
+  { name: "Terapia do Medo", 
+  image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQrFxpYCPPWiiOGG5pZttHFKywlfTzHHXPYKojERotZLpsiMO42", 
+  tickets: 115, 
+  category: "Terror" },
+
+  { name: "O Encontro de Carl", 
+  image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQWn1yIZNI-6zpCqPFLd4mn0suKix0BUqxSqlb8yM2oleD8td9w", 
+  tickets: 65, 
+  category: "Comédia" },
+
+  { name: "M3GAN", 
+  image: "https://dx35vtwkllhj9.cloudfront.net/universalstudios/megan/images/regions/us/onesheet.jpg", 
+  tickets: 135, 
+  category: "Terror" },
+
+  { name: "2067", 
+  image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQUy7Dq51oPVZwnVzeDZUGDJWPRsYaSB2sN5F7Q0T2loxbJ0E0e", 
+  tickets: 105, 
+  category: "Terror" },
+
+  { name: "Paralisia", 
+  image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQDv-CKwtsWxfwG1I7CBPgVz8J8leuP059DWIDSPSzO8TtLOHfW", 
+  tickets: 90, 
+  category: "Terror" }
 ];
 
-function createMovieElement(movie) {
-
-const movieDiv = document.createElement("Div");
-  movieDiv.className = "movie";
-
-const movieImage = document.createElement("img");
-  movieImage.src = movie.imageSrc;
-  movieImage.alt = movie.title;
-
-const movieTitle = document.createElement("h2");
-  movieTitle.textContent = movie.title;
-
-const movieCategory = document.createElement("p");
-  movieCategory.textContent = movie.category;
-
-const pagination = document.getElementById("pagination");
-
-    movieDiv.appendChild(movieImage);
-    movieDiv.appendChild(movieTitle);
-    movieDiv.appendChild(movieCategory);
-
-return movieDiv;
-}
+const movieList = document.getElementById("movieList");
+  const searchInput = document.getElementById("searchInput");
+  const categoryFilter = document.getElementById("categoryFilter");
+  const pagination = document.getElementById("pagination");
 
 function renderMovieList(pageNumber = 1) {
   const moviesPerPage = 10;
-  const startIndex = (pageNumber - 1)* moviesPerPage;
+  const startIndex = (pageNumber - 1) * moviesPerPage;
   const endIndex = startIndex + moviesPerPage;
+
+  const filteredMovies = movies.filter(movie => {
+  const searchValue = searchInput.value.toLowerCase();
+  const categoryValue = categoryFilter.value;
+
+return (
+  (movie.name.toLowerCase().includes(searchValue) || searchValue === "") &&
+  (categoryValue === "Todos" || movie.category === categoryValue)
+  );
+});
+
+const paginatedMovies = filteredMovies.slice(startIndex, endIndex);
+     
+ movieList.innerHTML = "";
+
+for (const movie of paginatedMovies) {
+   const movieCard = document.createElement("div");
+   movieCard.classList.add("movie-card");
+
+movieCard.innerHTML = `
+ <h2>${movie.name}</h2>
+  <img src="${movie.image}" alt="${movie.name}">
+  <p>Tickets disponíveis: ${movie.tickets}</p>
+  <p>Categoria: ${movie.category}</p>
+`;
+  
+  movieList.appendChild(movieCard);
+ }
+     
+  renderPagination(filteredMovies.length, pageNumber);
 }
 
 function renderPagination(totalMovies, currentPage) {
   pagination.innerHTML = "";
-
+     
   const totalPages = Math.ceil(totalMovies / 10);
+     
+for (let i = 1; i <= totalPages; i++) {
+  const pageButton = document.createElement("button");
+  pageButton.innerText = i;
+       
+if (i === currentPage) {
+  pageButton.disabled = true;
+}
 
-  for (let i = 1; i <=totalPages; i++) {
-    const pageButton = document.createElement("button");
-    pageButton.innerText = i;
-
-    if (i === currentPage) {
-      pageButton.disabled = true;
-    }
+pageButton.addEventListener("click", () => {
+  renderMovieList(i);
+    });
+       
+  pagination.appendChild(pageButton);
   }
 }
 
-const movieList = document.getElementById("movie-list");
-  movies.forEach((movie) => {
-    const movieElement = createMovieElement(movie);
-    movieList.appendChild(movieElement);
-});
+searchInput.addEventListener("input", () => renderMovieList());
+  categoryFilter.addEventListener("change", () => renderMovieList());
+
+renderMovieList();
